d143ffb0d34a86e396c84becfd2c276f62698fa9
Tobbe Gyllebring <i.am@drunkencoder.com>
2016-03-24 01:00:19 +0100

    Less passing around of that same thing.

--- DataBoss/ObjectReader.cs
+++ DataBoss/ObjectReader.cs
@@ -53,105 +53,112 @@ namespace DataBoss
 			}
 		}
 
+		class ConverterFactory
+		{
+			public ParameterExpression arg0 = Expression.Parameter(typeof(IDataRecord), "x");
+
+			Expression ReadField(Type fieldType, int ordinal) {
+				var recordType = fieldType;
+				var o = Expression.Constant(ordinal);
+				if (fieldType == typeof(string) || IsNullable(fieldType, ref recordType))
+					return Expression.Condition(
+						Expression.Call(arg0, IsDBNull, o),
+						Expression.Default(fieldType),
+						Convert(Expression.Call(arg0, GetGetMethod(arg0, recordType), o), fieldType));
+
+				return Convert(Expression.Call(arg0, GetGetMethod(arg0, fieldType), o), fieldType);
+			}
+
+			public Expression<Func<IDataRecord,T>> Converter<T>(FieldMap map) => Expression.Lambda<Func<IDataRecord,T>>(MemberInit(typeof(T), map), arg0);
+
+			static Expression Convert(Expression expr, Type targetType) {
+				return expr.Type == targetType
+					? expr
+					: Expression.Convert(expr, targetType);
+			} 
+			
+			Expression MemberInit(Type fieldType, FieldMap map) =>
+				Expression.MemberInit(
+					GetCtor(map, fieldType),
+					GetFields(map, fieldType)
+				);
+
+			NewExpression GetCtor(FieldMap map, Type fieldType)
+			{
+				var ctors = fieldType.GetConstructors()
+					.Select(ctor => new { ctor, p = ctor.GetParameters() })
+					.OrderByDescending(x => x.p.Length);
+				foreach(var item in ctors) {
+					var pn = new Expression[item.p.Length];
+					if(TryMaParameters(map, item.p, ReadField, pn))
+						return Expression.New(item.ctor, pn);
+				}
+
+				return Expression.New(fieldType);
+			}
+
+			static bool TryMaParameters(FieldMap map, ParameterInfo[] parameters, Func<Type, int, Expression> read, Expression[] exprs) {
+				int ordinal;
+				for(var i = 0; i != parameters.Length; ++i)
+					if(!map.TryGetOrdinal(parameters[i].Name, out ordinal))
+						return false;
+					else exprs[i] = read(parameters[i].ParameterType, ordinal);
+				return true;
+			} 
+
+			IEnumerable<MemberAssignment> GetFields(FieldMap fieldMap, Type targetType) {
+				return targetType
+					.GetFields()
+					.Where(x => !x.IsInitOnly)
+					.Select(x => {
+						var ordinal = 0;
+						if(fieldMap.TryGetOrdinal(x.Name, out ordinal))
+							return Expression.Bind(x, ReadField(x.FieldType, ordinal));
+
+						FieldMap subField;
+						if(fieldMap.TryGetSubMap(x.Name, out subField))
+							return Expression.Bind(x, MemberInit(x.FieldType, subField));
+
+						return null;
+					}).Where(x => x != null);
+			}
+
+			private static bool IsNullable(Type fieldType, ref Type recordType) {
+				var isNullable = fieldType.IsGenericType && fieldType.GetGenericTypeDefinition() == typeof(Nullable<>);
+				if(isNullable)
+					recordType = fieldType.GetGenericArguments()[0];
+				return isNullable;
+			}
+
+			private static MethodInfo GetGetMethod(Expression arg0, Type fieldType) {
+				var getter = arg0.Type.GetMethod("Get" + MapFieldType(fieldType));
+				if(getter != null)
+					return getter;
+
+				if(fieldType == typeof(byte[])) {
+					var getValue = arg0.Type.GetMethod("GetValue");
+					if(getValue != null)
+						return getValue;
+				}
+
+				throw new NotSupportedException("Can't read field of type:" + fieldType);
+			}
+
+			private static string MapFieldType(Type fieldType) {
+				switch(fieldType.FullName) {
+					case "System.Single": return "Float";
+					case "System.Object": return "Value";
+				}
+				return fieldType.Name;
+			}
+		}
+
 		public static Expression<Func<IDataRecord, T>> MakeConverter<T>(IDataReader reader) {
 			var fieldMap = new FieldMap();
 			for(var i = 0; i != reader.FieldCount; ++i)
 				fieldMap.Add(reader.GetName(i), i);
-
-			var arg0 = Expression.Parameter(typeof(IDataRecord), "x");
-			return Expression.Lambda<Func<IDataRecord, T>>(MemberInit(typeof(T), fieldMap, (field, n) => ReadField(arg0, field, Expression.Constant(n))), arg0);
-		}
-
-		static Expression MemberInit(Type fieldType, FieldMap map, Func<Type, int, Expression> read) =>
-			Expression.MemberInit(
-				GetCtor(map, fieldType, read),
-				GetFields(map, fieldType, read)
-			);
-
-		static NewExpression GetCtor(FieldMap map, Type fieldType, Func<Type, int, Expression> read)
-		{
-			var ctors = fieldType.GetConstructors()
-				.Select(ctor => new { ctor, p = ctor.GetParameters() })
-				.OrderByDescending(x => x.p.Length);
-			foreach(var item in ctors) {
-				var pn = new Expression[item.p.Length];
-				if(TryMaParameters(map, item.p, read, pn))
-					return Expression.New(item.ctor, pn);
-			}
-
-			return Expression.New(fieldType);
-		}
-
-		static bool TryMaParameters(FieldMap map, ParameterInfo[] parameters, Func<Type, int, Expression> read, Expression[] exprs) {
-			int ordinal;
-			for(var i = 0; i != parameters.Length; ++i)
-				if(!map.TryGetOrdinal(parameters[i].Name, out ordinal))
-					return false;
-				else exprs[i] = read(parameters[i].ParameterType, ordinal);
-			return true;
-		} 
-
-		static IEnumerable<MemberAssignment> GetFields(FieldMap fieldMap, Type targetType, Func<Type, int, Expression> read) {
-			return targetType
-				.GetFields()
-				.Where(x => !x.IsInitOnly)
-				.Select(x => {
-					var ordinal = 0;
-					if(fieldMap.TryGetOrdinal(x.Name, out ordinal))
-						return Expression.Bind(x, read(x.FieldType, ordinal));
-
-					FieldMap subField;
-					if(fieldMap.TryGetSubMap(x.Name, out subField))
-						return Expression.Bind(x, MemberInit(x.FieldType, subField, read));
-
-					return null;
-				}).Where(x => x != null);
-		}
-
-		static Expression ReadField(Expression arg0, Type fieldType, Expression ordinal) {
-			var recordType = fieldType;
-			if (fieldType == typeof(string) || IsNullable(fieldType, ref recordType))
-				return Expression.Condition(
-					Expression.Call(arg0, IsDBNull, ordinal),
-					Expression.Default(fieldType),
-					Convert(Expression.Call(arg0, GetGetMethod(arg0, recordType), ordinal), fieldType));
-
-			return Convert(Expression.Call(arg0, GetGetMethod(arg0, fieldType), ordinal), fieldType);
-		}
-
-		static Expression Convert(Expression expr, Type targetType) {
-			return expr.Type == targetType
-				? expr
-				: Expression.Convert(expr, targetType);
-		} 
-
-		private static bool IsNullable(Type fieldType, ref Type recordType) {
-			var isNullable = fieldType.IsGenericType && fieldType.GetGenericTypeDefinition() == typeof(Nullable<>);
-			if(isNullable)
-				recordType = fieldType.GetGenericArguments()[0];
-			return isNullable;
-		}
-
-		private static MethodInfo GetGetMethod(Expression arg0, Type fieldType) {
-			var getter = arg0.Type.GetMethod("Get" + MapFieldType(fieldType));
-			if(getter != null)
-				return getter;
-
-			if(fieldType == typeof(byte[])) {
-				var getValue = arg0.Type.GetMethod("GetValue");
-				if(getValue != null)
-					return getValue;
-			}
-
-			throw new NotSupportedException("Can't read field of type:" + fieldType);
-		}
-
-		private static string MapFieldType(Type fieldType) {
-			switch(fieldType.FullName) {
-				case "System.Single": return "Float";
-				case "System.Object": return "Value";
-			}
-			return fieldType.Name;
+			var rr = new ConverterFactory();
+			return rr.Converter<T>(fieldMap);
 		}
 	}
 }
