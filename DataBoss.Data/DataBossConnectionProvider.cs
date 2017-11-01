dd963a75f5419d160c5c62cbfbe6934d6f883989
Tobbe Gyllebring <tobbe@drunkcod.com>
2017-11-01 13:10:26 +0100

    Friendlier named property types.

--- DataBoss.Data/DataBossConnectionProvider.cs
+++ DataBoss.Data/DataBossConnectionProvider.cs
@@ -10,6 +10,28 @@ namespace DataBoss.Data
 {
 	public class DataBossConnectionProvider
 	{
+		public struct ByteSize
+		{
+			static string[] Unit = new[] { "KiB", "MiB", "GiB", "TiB", "PiB" };
+			static long[] Threshold = new[] { 1L << 10, 1L << 20, 1L << 30, 1L << 40, 1L << 50 };
+			public readonly long TotalBytes;
+
+			public ByteSize(long totalBytes) { this.TotalBytes = totalBytes; }
+
+			public static implicit operator long(ByteSize size) => size.TotalBytes;
+
+			public override string ToString() {
+				var unit = 0;
+				for (; unit < Threshold.Length && Threshold[unit] < TotalBytes; ++unit)
+					;
+
+				if (unit == 0)
+					return TotalBytes.ToString();
+				unit -= 1;
+				return string.Format("{0:N2} {1}", (1.0 * TotalBytes) / Threshold[unit], Unit[unit]);
+			}
+		}
+
 		public struct ProviderStatistics : IEnumerable<KeyValuePair<string, long>>
 		{
 			readonly Dictionary<string, long> stats;
@@ -18,11 +40,12 @@ namespace DataBoss.Data
 				this.stats = stats;
 			}
 
-			public long BytesReceived => GetOrDefault(nameof(BytesReceived));
+			public ByteSize BytesReceived => new ByteSize(GetOrDefault(nameof(BytesReceived)));
 			public long ConnectionsCreated => GetOrDefault(nameof(ConnectionsCreated));
 			public long LiveConnections => GetOrDefault(nameof(LiveConnections));
 			public long SelectCount => GetOrDefault(nameof(SelectCount));
 			public long SelectRows => GetOrDefault(nameof(SelectRows));
+			public TimeSpan ExecutionTime => TimeSpan.FromMilliseconds(GetOrDefault(nameof(ExecutionTime)));
 
 			public long this[string key] => stats[key];
 			public IEnumerator<KeyValuePair<string, long>> GetEnumerator() => stats.GetEnumerator();
