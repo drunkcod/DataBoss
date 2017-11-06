8333a9cc8649ab533cd2b18ccf4c7f3b2722273b
Tobbe Gyllebring <tobbe@drunkcod.com>
2017-11-06 13:23:16 +0100

    Moved props to the global props thing. Fixes the build path issues.

--- Package-Release.ps1
+++ Package-Release.ps1
@@ -1,7 +1,6 @@
 $Build = [Regex]::Match([IO.File]::ReadAllText("DataBoss\Properties\Version.cs"), "AssemblyInformationalVersion\(\""(?<build>.+)\""\)").Groups["build"].Value
 
-& dotnet msbuild DataBoss\DataBoss.sln /t:Clean /p:Configuration=Release /v:m /nologo
-& dotnet msbuild Build.proj /t:Build /p:Configuration=Release /p:Build=$Build /v:m /nologo
+& dotnet msbuild Build.proj "/t:Clean;Restore;Build" /p:Configuration=Release /p:Build=$Build /v:m /nologo
 
 & dotnet msbuild DataBoss\DataBoss.csproj /t:Pack /p:OutputDirectory=Artifacts /p:Configuration=Release /p:Build=$Build /p:PackageVersion=$Build
 & dotnet msbuild DataBoss.Data/DataBoss.Data.csproj /t:Pack /p:Configuration=Release /p:Build=$Build /p:PackageVersion=$Build /v:m /nologo 
