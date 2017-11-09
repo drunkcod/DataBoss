81309f21ee2960e6951063306873c52c8afca312
Tobbe Gyllebring <tobbe@drunkcod.com>
2017-11-09 12:53:53 +0100

    Package-Release now does fully clean build.

--- Package-Release.ps1
+++ Package-Release.ps1
@@ -1,6 +1,7 @@
 $Build = [Regex]::Match([IO.File]::ReadAllText("DataBoss\Properties\Version.cs"), "AssemblyInformationalVersion\(\""(?<build>.+)\""\)").Groups["build"].Value
 
-& dotnet msbuild Build.proj "/t:Clean;Restore;Build" /p:Configuration=Release /p:Build=$Build /v:m /nologo
+Remove-Item -Path Build -Recurse -Force
+& dotnet msbuild Build.proj /t:Restore /t:Build /p:Configuration=Release /p:Build=$Build /v:m /nologo
 
 Write-Host Packgaging version $Build
 & dotnet msbuild DataBoss.Data/DataBoss.Data.csproj /t:Pack /p:Configuration=Release /p:Build=$Build /p:PackageVersion=$Build /v:m /nologo /p:DataBossPackageVersion=$Build /p:PackageOutputPath="..\Build"
