40e1c9dc7874dd46e8ed362e585fab1254da7713
Tobbe Gyllebring <tobbe@drunkcod.com>
2017-11-09 11:30:11 +0100

    Package the things with less fiddling. And more magic.

--- DataBoss/Properties/Version.cs
+++ DataBoss/Properties/Version.cs
@@ -1,5 +1,5 @@
 using System.Reflection;
 
-[assembly: AssemblyVersion("0.0.0.40")]
-[assembly: AssemblyFileVersion("2017.11.07")]
-[assembly: AssemblyInformationalVersion("0.0.40")]
+[assembly: AssemblyVersion("0.0.0.41")]
+[assembly: AssemblyFileVersion("2017.11.09")]
+[assembly: AssemblyInformationalVersion("0.0.41")]
--- Package-Release.ps1
+++ Package-Release.ps1
@@ -2,5 +2,6 @@ $Build = [Regex]::Match([IO.File]::ReadAllText("DataBoss\Properties\Version.cs")
 
 & dotnet msbuild Build.proj "/t:Clean;Restore;Build" /p:Configuration=Release /p:Build=$Build /v:m /nologo
 
-& dotnet msbuild DataBoss\DataBoss.csproj /t:Pack /p:OutputDirectory=Artifacts /p:Configuration=Release /p:Build=$Build /p:PackageVersion=$Build
-& dotnet msbuild DataBoss.Data/DataBoss.Data.csproj /t:Pack /p:Configuration=Release /p:Build=$Build /p:PackageVersion=$Build /v:m /nologo 
+Write-Host Packgaging version $Build
+& dotnet msbuild DataBoss.Data/DataBoss.Data.csproj /t:Pack /p:Configuration=Release /p:Build=$Build /p:PackageVersion=$Build /v:m /nologo /p:DataBossPackageVersion=$Build /p:PackageOutputPath="..\Build"
+& dotnet msbuild DataBoss\DataBoss.csproj /t:Restore /t:Pack /p:Configuration=Release /p:Build=$Build /p:PackageVersion=$Build /p:DataBossPackageVersion=$Build /p:PackageOutputPath="..\Build"
