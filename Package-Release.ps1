b5b190fe0f4aef704eb1c487da6a72269687dc7b
Tobbe Gyllebring <tobbe@drunkcod.com>
2017-11-09 13:43:41 +0100

    Cleanup of package script.

--- Package-Release.ps1
+++ Package-Release.ps1
@@ -1,8 +1,26 @@
 $Build = [Regex]::Match([IO.File]::ReadAllText("DataBoss\Properties\Version.cs"), "AssemblyInformationalVersion\(\""(?<build>.+)\""\)").Groups["build"].Value
 
+function Args {
+    param([parameter(ValueFromRemainingArguments=$true)] [String[]] $args)
+    $args
+}
+
+$BuildArgs = Args /v:m /nologo /p:Configuration=Release /p:Build=$Build
+$PackArgs = $BuildArgs + (Args /t:Pack /p:PackageVersion=$Build /p:DataBossPackageVersion=$Build /p:PackageOutputPath="..\Build")
+
+function MSBuild {
+    param([parameter(ValueFromRemainingArguments=$true)] [String[]] $args)
+    & dotnet msbuild ($args + $BuildArgs)
+}
+
+function NuPack {
+    param([parameter(ValueFromRemainingArguments=$true)] [String[]] $args)
+    & dotnet msbuild ($args + $PackArgs) 
+}
+
 Remove-Item -Path Build -Recurse -Force
-& dotnet msbuild Build.proj /t:Restore /t:Build /p:Configuration=Release /p:Build=$Build /v:m /nologo
+MSBuild Build.proj /t:Restore /t:Build /p:Configuration=Release /p:Build=$Build
 
 Write-Host Packgaging version $Build
-& dotnet msbuild DataBoss.Data/DataBoss.Data.csproj /t:Pack /p:Configuration=Release /p:Build=$Build /p:PackageVersion=$Build /v:m /nologo /p:DataBossPackageVersion=$Build /p:PackageOutputPath="..\Build"
-& dotnet msbuild DataBoss\DataBoss.csproj /t:Restore /t:Pack /p:Configuration=Release /p:Build=$Build /p:PackageVersion=$Build /p:DataBossPackageVersion=$Build /p:PackageOutputPath="..\Build"
+NuPack DataBoss.Data/DataBoss.Data.csproj 
+NuPack DataBoss\DataBoss.csproj /t:Restore
