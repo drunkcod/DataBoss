14ab50cf8a857f0d8ba90767bedbf6d4339af483
Tobbe Gyllebring <tobbe@drunkcod.com>
2017-11-22 22:27:29 +0100

    it's a bit hackey but we got tests again.

--- DataBoss/Properties/Version.cs
+++ DataBoss/Properties/Version.cs
@@ -1,5 +1,5 @@
 using System.Reflection;
 
-[assembly: AssemblyVersion("0.0.0.42")]
-[assembly: AssemblyFileVersion("2017.11.16")]
-[assembly: AssemblyInformationalVersion("0.0.42")]
+[assembly: AssemblyVersion("0.0.0.43")]
+[assembly: AssemblyFileVersion("2017.11.22")]
+[assembly: AssemblyInformationalVersion("0.0.43")]
--- Run-Tests.bat
+++ Run-Tests.bat
@@ -1,2 +1,4 @@
 @echo off
-Build\packages\Cone.Conesole.2017.8.1.2\tools\net452\Conesole.exe Build\DataBoss.Specs\Debug\DataBoss.Specs.dll %*
+pushd DataBoss.Specs
+dotnet conesole .\..\Build\DataBoss.Specs\Debug\net45\DataBoss.Specs.dll %*
+popd
