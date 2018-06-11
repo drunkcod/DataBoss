aada9f2e767e625a21ae4c8a5c313ec659ada5ec
Tobbe Gyllebring <tobbe@drunkcod.com>
2018-06-11 10:11:27 +0200

    RowVersion now also works as intended via SequenceDataReader which enables Insert/Into scenarios.

--- DataBoss/Properties/Version.cs
+++ DataBoss/Properties/Version.cs
@@ -1,5 +1,5 @@
 using System.Reflection;
 
 [assembly: AssemblyFileVersion("2018.06.11")]
-[assembly: AssemblyVersion("0.0.0.51")]
-[assembly: AssemblyInformationalVersion("0.0.51")]
+[assembly: AssemblyVersion("0.0.0.52")]
+[assembly: AssemblyInformationalVersion("0.0.52")]
