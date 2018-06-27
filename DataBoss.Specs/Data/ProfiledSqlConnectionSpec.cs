72e8989e76c58ee31c2c4a06d2819d25ee6c34bc
Tobbe Gyllebring <tobbe@drunkcod.com>
2018-06-27 23:46:20 +0200

    Basic reader support and full delegation.

--- DataBoss.Specs/Data/ProfiledSqlConnectionSpec.cs
+++ DataBoss.Specs/Data/ProfiledSqlConnectionSpec.cs
@@ -3,12 +3,15 @@ using System.Collections.Generic;
 using System.Data;
 using System.Data.Common;
 using System.Data.SqlClient;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using Cone;
 using Cone.Helpers;
+using DataBoss.Data;
+using DataBoss.Data.Common;
 
 namespace DataBoss.Specs.Data
 {
@@ -32,34 +35,49 @@ namespace DataBoss.Specs.Data
 				set => inner.CommandText = value;
 			}
 
-			public override int CommandTimeout { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-			public override CommandType CommandType { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-			public override bool DesignTimeVisible { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-			public override UpdateRowSource UpdatedRowSource { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-			protected override DbConnection DbConnection { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-
-			protected override DbParameterCollection DbParameterCollection => throw new NotImplementedException();
-
-			protected override DbTransaction DbTransaction { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-
-			public override void Cancel() {
-				throw new NotImplementedException();
+			public override int CommandTimeout { 
+				get => inner.CommandTimeout; 
+				set => inner.CommandTimeout = value; 
 			}
+
+			public override CommandType CommandType { 
+				get => inner.CommandType; 
+				set => inner.CommandType = value;
+			}
+
+			public override bool DesignTimeVisible { 
+				get => inner.DesignTimeVisible; 
+				set => inner.DesignTimeVisible = value; 
+			}
+
+			public override UpdateRowSource UpdatedRowSource { 
+				get => inner.UpdatedRowSource; 
+				set => inner.UpdatedRowSource = value; 
+			}
+
+			protected override DbConnection DbConnection { 
+				get => parent; 
+				set => throw new NotSupportedException("Can't switch connection for profiled command");
+			}
+
+			protected override DbParameterCollection DbParameterCollection => inner.Parameters;
+
+			protected override DbTransaction DbTransaction { 
+				get => inner.Transaction;
+				set => throw new NotSupportedException("Can't assign transaction to profiled command");
+			}
+
+			public override void Cancel() => inner.Cancel();
 
 			public override int ExecuteNonQuery() => parent.Execute(this, DoExecuteNonQuery);
 			public override object ExecuteScalar() => parent.Execute(this, DoExecuteScalar);
+			protected override DbDataReader ExecuteDbDataReader(CommandBehavior behavior) => 
+				parent.Execute(this, x => x.inner.ExecuteReader(behavior));
 
-			public override void Prepare() {
-				throw new NotImplementedException();
-			}
+			public override void Prepare() => inner.Prepare();
 
-			protected override DbParameter CreateDbParameter() {
-				throw new NotImplementedException();
-			}
+			protected override DbParameter CreateDbParameter() => inner.CreateParameter();
 
-			protected override DbDataReader ExecuteDbDataReader(CommandBehavior behavior) {
-				throw new NotImplementedException();
-			}
 		}
 
 		readonly SqlConnection inner;
@@ -68,46 +86,37 @@ namespace DataBoss.Specs.Data
 			this.inner = inner;
 		}
 
-		public override string ConnectionString { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-
-		public override string Database => throw new NotImplementedException();
-
-		public override string DataSource => throw new NotImplementedException();
-
-		public override string ServerVersion => throw new NotImplementedException();
-
-		public override ConnectionState State => throw new NotImplementedException();
+		public override string ConnectionString { 
+			get => inner.ConnectionString; 
+			set => inner.ConnectionString = value; 
+		}
+	
+		public override string Database => inner.Database;
+		public override string DataSource => inner.DataSource;
+		public override string ServerVersion => inner.ServerVersion;
+		public override ConnectionState State => inner.State;
 
 		public event EventHandler<ProfiledCommandExecutingEventArgs> CommandExecuting;
 		public event EventHandler<ProfiledCommandExecutedEventArgs> CommandExecuted;
 
 		protected override DbCommand CreateDbCommand() => new ProfiledSqlCommand(this, inner.CreateCommand());
-
-		protected override DbTransaction BeginDbTransaction(IsolationLevel isolationLevel) {
-			throw new NotImplementedException();
-		}
-
-		public override void Close() {
-			throw new NotImplementedException();
-		}
-
-		public override void ChangeDatabase(string databaseName) {
-			throw new NotImplementedException();
-		}
-
+		protected override DbTransaction BeginDbTransaction(IsolationLevel isolationLevel) => inner.BeginTransaction(isolationLevel);
+		public override void Close() => inner.Close();
+		public override void ChangeDatabase(string databaseName) => inner.ChangeDatabase(databaseName);
 		public override void Open() => inner.Open();
 		public override Task OpenAsync(CancellationToken cancellationToken) => inner.OpenAsync(cancellationToken);
 
 		T Execute<T>(ProfiledSqlCommand command, Func<ProfiledSqlCommand, T> @do) {
 			OnExecuting(command);
+			var t = Stopwatch.StartNew();
 			var r = @do(command);
-			OnExecuted(command);
+			t.Stop();
+			OnExecuted(command, t.Elapsed);
 			return r;
 		}
 
 		void OnExecuting(ProfiledSqlCommand command) => CommandExecuting?.Invoke(this, new ProfiledCommandExecutingEventArgs(command));
-		void OnExecuted(ProfiledSqlCommand command) => CommandExecuted?.Invoke(this, new ProfiledCommandExecutedEventArgs(command));
-
+		void OnExecuted(ProfiledSqlCommand command, TimeSpan elapsed) => CommandExecuted?.Invoke(this, new ProfiledCommandExecutedEventArgs(command, elapsed));
 	}
 
 	public class ProfiledCommandExecutingEventArgs : EventArgs 
@@ -122,9 +131,11 @@ namespace DataBoss.Specs.Data
 	public class ProfiledCommandExecutedEventArgs : EventArgs
 	{
 		public readonly DbCommand Command;
+		public readonly TimeSpan Elapsed;
 
-		public ProfiledCommandExecutedEventArgs(DbCommand command) {
+		public ProfiledCommandExecutedEventArgs(DbCommand command, TimeSpan elapsed) {
 			this.Command = command;
+			this.Elapsed = elapsed;
 		}
 	}
 
@@ -170,11 +181,25 @@ namespace DataBoss.Specs.Data
 				() => commandExecuting.HasBeenCalled);
 		}
 
+		public void listens_to_ExecuteReader() {
+			var expectedCommand = "select Id = 2 union all select 3";
+			var commandExecuting = new EventSpy<ProfiledCommandExecutingEventArgs>(
+				(s, e) => Check.That(() => e.Command.CommandText == expectedCommand));
+			con.CommandExecuting += commandExecuting;
+
+			var q = con.CreateCommand();
+			q.CommandText = expectedCommand;
+
+			Check.With(() => ObjectReader.For(q.ExecuteReader()).Read<IdRow<int>>().ToList())
+				.That(r => r.Count == 2, _ => commandExecuting.HasBeenCalled);
+		}
+
 		public void CommandExecuted_after_CommandExecuting() {
 			var commandExecuting = new EventSpy<ProfiledCommandExecutingEventArgs>();
 			con.CommandExecuting += commandExecuting;
 
-			var commandExecuted = new EventSpy<ProfiledCommandExecutedEventArgs>();
+			var commandExecuted = new EventSpy<ProfiledCommandExecutedEventArgs>((_, e) => 
+				Check.That(() => e.Elapsed > TimeSpan.Zero));
 			con.CommandExecuted += commandExecuted;
 
 			var q = con.CreateCommand();
@@ -182,7 +207,6 @@ namespace DataBoss.Specs.Data
 			q.ExecuteScalar();
 
 			Check.That(() => commandExecuting.CalledBefore(commandExecuted));
-
 		}
 	}
 }
