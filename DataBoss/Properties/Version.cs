d93f87202b1878e7b28e407701cdb7f3330fc6b4
Tobbe Gyllebring <tobbe@drunkcod.com>
2018-12-09 12:38:50 +0100

    Bump.

--- DataBoss/Properties/Version.cs
+++ DataBoss/Properties/Version.cs
@@ -1,5 +1,5 @@
 using System.Reflection;
 
-[assembly: AssemblyFileVersion("2018.11.28")]
-[assembly: AssemblyVersion("0.0.0.62")]
-[assembly: AssemblyInformationalVersion("0.0.62")]
+[assembly: AssemblyFileVersion("2018.12.09")]
+[assembly: AssemblyVersion("0.0.0.63")]
+[assembly: AssemblyInformationalVersion("0.0.63")]
