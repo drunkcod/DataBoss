163a5d126da3b26d20d54dcdc6846a95df196fac
Tobbe Gyllebring <tobbe@drunkcod.com>
2019-11-07 16:53:42 +0100

    CLI packaging.

--- Package-Release.ps1
+++ Package-Release.ps1
@@ -6,18 +6,28 @@ function Args {
 $BuildArgs = Args /v:m /nologo /p:Configuration=Release
 $PackArgs = $BuildArgs + (Args /t:Pack /p:PackageOutputPath="..\Build")
 
-function MSBuild {
-    param([parameter(ValueFromRemainingArguments=$true)] [String[]] $args)
-    & dotnet msbuild ($args + $BuildArgs)
-}
-
 function NuPack {
     param([parameter(ValueFromRemainingArguments=$true)] [String[]] $args)
     & dotnet msbuild ($args + $PackArgs) 
 }
 
-dotnet msbuild  DataBoss\DataBoss.sln /t:Restore /t:Clean /t:Build
+dotnet msbuild ((Args DataBoss\DataBoss.sln /t:Restore /t:Clean /t:Build) + $BuildArgs)
 NuPack DataBoss.Linq/DataBoss.Linq.csproj
 NuPack DataBoss.Data/DataBoss.Data.csproj 
 NuPack DataBoss.DataPackage/DataBoss.DataPackage.csproj
 NuPack DataBoss\DataBoss.csproj
+
+#CLI Packaging
+$v = (Get-ChildItem Build\DataBoss.Cli\Release\net452\DataBoss.dll | Select-Object -ExpandProperty VersionInfo).ProductVersion
+$p = "Build\Bin\DastaBoss.Cli-$v"
+
+& { param($p)
+    If (Test-Path $p) { Remove-Item $p -Recurse -Force }
+} Build\Bin
+dotnet publish DataBoss.Cli\DataBoss.Cli.csproj --nologo --no-build --configuration Release /p:TargetFramework=netcoreapp2.1 -o $p\netcoreapp2.1
+Copy-Item Build\DataBoss.Cli\Release\net452 -Destination $p -Recurse -Force
+$$ = @{
+    Path = $p
+    DestinationPath = "$p.zip"
+}
+Compress-Archive @$ -Force
