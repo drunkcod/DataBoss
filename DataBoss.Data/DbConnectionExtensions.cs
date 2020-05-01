228c97c15068f75f0f927c5c1aa2975354a22ec9
Tobbe Gyllebring <tobbe@drunkcod.com>
2020-05-01 14:09:58 +0200

    DbConnectionExtensions in commons.

--- DataBoss.Data/DbConnectionExtensions.cs
+++ DataBoss.Data/DbConnectionExtensions.cs
@@ -1,25 +1,83 @@
-#if MSSQLCLIENT
-namespace DataBoss.Data.MsSql
-{
-	using Microsoft.Data.SqlClient;
-#else
 namespace DataBoss.Data
 {
-	using System.Data.SqlClient;
-#endif
-
 	using System;
 	using System.Collections.Concurrent;
 	using System.Collections.Generic;
 	using System.Data;
+	using System.Data.Common;
 	using System.Linq;
+	using System.Linq.Expressions;
 
 	public static class DbConnectionExtensions
 	{
-		static IDataBossConnection Wrap(IDbConnection db) => DbConnectionCoreExtensions.GetExtras(db);
+		static Func<IDbConnection, IDataBossConnection> WrapConnection = _ => null;
+		static readonly ConcurrentBag<Delegate> connectionWrapers = new ConcurrentBag<Delegate>();
+
+		public static void Register<T>(Func<T, IDataBossConnection> wrap) =>
+			connectionWrapers.Add(wrap);
+
+		public static void DisposeOnClose(this DbConnection connection) {
+			connection.StateChange += DisposeOnClose;
+		}
+
+		static void DisposeOnClose(object obj, StateChangeEventArgs e) {
+			if (e.CurrentState == ConnectionState.Closed) {
+				var c = (DbConnection)obj;
+				c.StateChange -= DisposeOnClose;
+				c.Dispose();
+			}
+		}
+
+		public static IDbCommand CreateCommand(this IDbConnection connection, string commandText) {
+			var c = connection.CreateCommand();
+			c.CommandText = commandText;
+			return c;
+		}
+
+		public static IDbCommand CreateCommand<T>(this IDbConnection connection, string cmdText, T args) =>
+			Wrap(connection).CreateCommand(cmdText, args);
+
+		public static object ExecuteScalar<T>(this IDbConnection connection, string commandText, T args) =>
+		CreateCommand(connection, commandText, args).Use(DbOps<IDbCommand, IDataReader>.ExecuteScalar);
+
+		public static int ExecuteNonQuery<T>(this IDbConnection connection, string commandText, T args) =>
+			CreateCommand(connection, commandText, args).Use(DbOps<IDbCommand, IDataReader>.ExecuteNonQuery);
+
+		public static object ExecuteScalar(this IDbConnection connection, string commandText) =>
+			CreateCommand(connection, commandText).Use(DbOps<IDbCommand, IDataReader>.ExecuteScalar);
+
+		public static int ExecuteNonQuery(this IDbConnection connection, string commandText) =>
+			CreateCommand(connection, commandText).Use(DbOps<IDbCommand, IDataReader>.ExecuteNonQuery);
+
+		public static void Into<T>(this IDbConnection connection, string destinationTable, IEnumerable<T> rows) =>
+			Into(connection, destinationTable, rows, new DataBossBulkCopySettings());
+
+		public static void Into<T>(this IDbConnection connection, string destinationTable, IEnumerable<T> rows, DataBossBulkCopySettings settings) =>
+			Into(connection, destinationTable, SequenceDataReader.Create(rows, x => x.MapAll()), settings);
+
+		public static void Into(this IDbConnection connection, string destinationTable, IDataReader rows) =>
+			Into(connection, destinationTable, rows, new DataBossBulkCopySettings());
+
+		public static void Into(this IDbConnection connection, string destinationTable, IDataReader rows, DataBossBulkCopySettings settings) {
+			var extras = Wrap(connection);
+			extras.CreateTable(destinationTable, rows);
+			extras.Insert(destinationTable, rows, settings);
+		}
+
+		public static void Insert<T>(this IDbConnection connection, string destinationTable, IEnumerable<T> rows) =>
+			Insert(connection, destinationTable, rows, new DataBossBulkCopySettings());
+
+		public static void Insert<T>(this IDbConnection connection, string destinationTable, IEnumerable<T> rows, DataBossBulkCopySettings settings) =>
+			Insert(connection, destinationTable, SequenceDataReader.Create(rows, x => x.MapAll()), settings);
+
+		public static void Insert(this IDbConnection connection, string destinationTable, IDataReader rows, DataBossBulkCopySettings settings) =>
+			Wrap(connection).Insert(destinationTable, rows, settings);
+
+		public static void CreateTable(this IDbConnection connection, string tableName, IDataReader data) =>
+			Wrap(connection).CreateTable(tableName, data);
 
 		static ConcurrentDictionary<Type, Action<IDbCommand, object>> CommandFactory = new ConcurrentDictionary<Type, Action<IDbCommand, object>>();
-		
+
 		public static IEnumerable<T> Query<T>(this IDbConnection db, string sql, object args = null, bool buffered = true) => Query<T>(db, sql, new DataBossQueryOptions { Parameters = args, Buffered = buffered });
 		public static IEnumerable<TResult> Query<T, TResult>(this IDbConnection db, Func<T, TResult> selector, string sql, object args = null, bool buffered = true) => Query(Wrap(db), sql, args, buffered).Read(selector);
 		public static IEnumerable<TResult> Query<T1, T2, TResult>(this IDbConnection db, Func<T1, T2, TResult> selector, string sql, object args = null, bool buffered = true) => Query(Wrap(db), sql, args, buffered).Read(selector);
@@ -62,7 +120,7 @@ namespace DataBoss.Data
 					AddParameters(Options.Parameters.GetType(), Connection.Dialect)(cmd, Options.Parameters);
 				return cmd;
 			}
-	
+
 			public IEnumerable<T> Read<T>() => BufferOrNot(DbObjectQuery.Create(GetCommand).Read<T>());
 
 			public IEnumerable<TResult> Read<T1, TResult>(Func<T1, TResult> selector) => BufferOrNot(DbObjectQuery.Create(GetCommand).Read(selector));
@@ -76,19 +134,19 @@ namespace DataBoss.Data
 			IEnumerable<T> BufferOrNot<T>(IEnumerable<T> xs) => Options.Buffered ? xs.ToList() : xs;
 
 			static Action<IDbCommand, object> AddParameters(Type t, ISqlDialect dialect) =>
-				CommandFactory.GetOrAdd(t, 
+				CommandFactory.GetOrAdd(t,
 					type => (Action<IDbCommand, object>)ToParams.CreateExtractor(dialect, typeof(IDbCommand), type, typeof(object)).Compile());
 		}
 
 		public static IDbConnection WithCommandTimeout(this IDbConnection db, int commandTimeout) =>
 			new DbConnectionDecorator(db, Wrap(db).Dialect) { CommandTimeout = commandTimeout };
-		
+
 		class DbConnectionDecorator : IDbConnection, IDataBossConnection
 		{
 			readonly IDbConnection InnerConnection;
-			
 
-			public DbConnectionDecorator(IDbConnection inner, ISqlDialect dialect) { 
+
+			public DbConnectionDecorator(IDbConnection inner, ISqlDialect dialect) {
 				this.InnerConnection = inner;
 				this.Dialect = dialect;
 			}
@@ -96,9 +154,9 @@ namespace DataBoss.Data
 			public ISqlDialect Dialect { get; }
 			public int? CommandTimeout;
 
-			public string ConnectionString { 
-				get => InnerConnection.ConnectionString; 
-				set => InnerConnection.ConnectionString = value; 
+			public string ConnectionString {
+				get => InnerConnection.ConnectionString;
+				set => InnerConnection.ConnectionString = value;
 			}
 
 			public int ConnectionTimeout => InnerConnection.ConnectionTimeout;
@@ -122,16 +180,16 @@ namespace DataBoss.Data
 			public void CreateTable(string destinationTable, IDataReader rows) =>
 				InnerConnection.CreateTable(destinationTable, rows);
 
-			public void Insert(string destinationTable, IDataReader rows, DataBossBulkCopySettings settings) => 
+			public void Insert(string destinationTable, IDataReader rows, DataBossBulkCopySettings settings) =>
 				InnerConnection.Insert(destinationTable, rows, settings.CommandTimeout.HasValue ? settings : settings.WithCommandTimeout(CommandTimeout));
 
-			public IDbCommand CreateCommand() => 
+			public IDbCommand CreateCommand() =>
 				Adorn(InnerConnection.CreateCommand());
 
 			public IDbCommand CreateCommand(string cmdText) =>
 				Adorn(InnerConnection.CreateCommand(cmdText));
 
-			public IDbCommand CreateCommand<T>(string cmdText, T args) => 
+			public IDbCommand CreateCommand<T>(string cmdText, T args) =>
 				Adorn(InnerConnection.CreateCommand(cmdText, args));
 
 			IDbCommand Adorn(IDbCommand c) {
@@ -139,6 +197,52 @@ namespace DataBoss.Data
 					c.CommandTimeout = CommandTimeout.Value;
 				return c;
 			}
+		}
+
+		public static IDataBossConnection Wrap(IDbConnection connection) {
+			var x = WrapConnection(connection);
+			if(x != null)
+				return x;
+			
+			if(connection is IDataBossConnection db)
+				return db;
+
+			BuildWrapperFactory();
+			return WrapConnection(connection) ?? throw new NotSupportedException(connection.GetType().Name);
+		}
+
+		static void BuildWrapperFactory()
+		{
+			var con = Expression.Parameter(typeof(IDbConnection), "c");
+
+			Expression body = Expression.Constant(null, typeof(IDataBossConnection));
+			foreach(var item in new[]{
+				"DataBoss.Data.MsSqlClient.DataBossSqlConnection, DataBoss.Data.MsSqlClient",
+				"DataBoss.Data.DataBossSqlConnection, DataBoss.Data.SqlClient", }) {
+
+				var type = Type.GetType(item);
+				if(type == null)
+					continue;
+				var ctor = type.GetConstructors().Single();
+				var asTarget = Expression.TypeAs(con, ctor.GetParameters()[0].ParameterType);
+				body = Expression.Condition(
+					Expression.ReferenceEqual(asTarget, Expression.Constant(null)), 
+					body,
+					Expression.Convert(Expression.New(ctor, asTarget), typeof(IDataBossConnection)));
+			}
+
+			foreach(var item in connectionWrapers.Reverse()) {
+				var asTarget = Expression.TypeAs(con, item.Method.GetParameters().Single().ParameterType);
+				body = Expression.Condition(
+					Expression.ReferenceEqual(asTarget, Expression.Constant(null)),
+					body,
+					Expression.Convert(Expression.Call(
+						Expression.Constant(item.Target), item.Method, asTarget), typeof(IDataBossConnection)));
+			}
+
+			WrapConnection = Expression.Lambda<Func<IDbConnection, IDataBossConnection>>(body, con)
+				.Compile();
+
 		}
 	}
 }
