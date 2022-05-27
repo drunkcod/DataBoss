e6ac9cca2f6546904f87dcb2e9086c18184151e1
Tobbe Gyllebring <tobbe@drunkcod.com>
2022-05-27 10:09:53 +0200

    Json/Jsonb parameter convienence.

---
+++ Source/DataBoss.Data.Npgsql/NpgsqlParamerCollectionExtensions.cs
@@ -0,0 +1,19 @@
+using Npgsql;
+using NpgsqlTypes;
+
+namespace DataBoss.Data
+{
+	public static class NpgsqlParamerCollectionExtensions
+	{
+		public static void AddJson<T>(this NpgsqlParameterCollection self, string name, T value) =>
+			self.Add(Utf8JsonParameter(name, NpgsqlDbType.Json, value));
+
+		public static void AddJsonb<T>(this NpgsqlParameterCollection self, string name, T value) =>
+			self.Add(Utf8JsonParameter(name, NpgsqlDbType.Jsonb, value));
+
+		static NpgsqlParameter<byte[]> Utf8JsonParameter<T>(string name, NpgsqlDbType type, T value) =>
+			new NpgsqlParameter<byte[]>(name, type) {
+				TypedValue = System.Text.Json.JsonSerializer.SerializeToUtf8Bytes(value),
+			};
+	}
+}
